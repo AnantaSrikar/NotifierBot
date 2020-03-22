c14b97a09af1492075c38b5196610829eb9d2777
Ananta Srikar <srikarananta01@gmail.com>
2020-03-22 17:21:08 +0530

    Added worldometer, other fix pending

--- scrapeHelperBoi.py
+++ scrapeHelperBoi.py
@@ -9,6 +9,11 @@ import re
 import os
 #for generating dates within a start and an end date
 
+def writeForWorldo(data):
+	fileManager = open('Worldometer.txt', 'w')
+	fileManager.write(data)
+	fileManager.close()
+
 START_DATE = 22
 START_MONTH = 2
 END_DATE = 17
@@ -18,10 +23,10 @@ start_date = date(2020, START_MONTH, START_DATE)
 end_date = date(2020, END_MONTH, END_DATE)
 states = ["Andhra Pradesh","Arunachal Pradesh ","Assam","Bihar","Chhattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal","Andaman and Nicobar Islands","Chandigarh","Dadra and Nagar Haveli","Daman and Diu","Lakshadweep","Delhi","Puducherry"]
 def generate_dates(start_date, end_date):
-    for n in range(int ((end_date - start_date).days)):
-        yield start_date + timedelta(n)
-    for single_date in daterange(start_date, end_date):
-        dates_list.append(single_date.strftime("%m-%d-%Y"))
+	for n in range(int ((end_date - start_date).days)):
+		yield start_date + timedelta(n)
+	for single_date in daterange(start_date, end_date):
+		dates_list.append(single_date.strftime("%m-%d-%Y"))
 #define all urls
 moh_url = 'https://www.mohfw.gov.in/'
 worldo_url = 'https://www.worldometers.info/coronavirus/'
@@ -31,87 +36,93 @@ jhu_daily_url = 'https://github.com/CSSEGISandData/COVID-19/tree/master/csse_cov
 header=["Sno","State","Cases"]
 #generate_dates()
 #DECCAN HERALD
-web_response = requests.get(dh_url)
-txt_to_parse = web_response.text
-x = txt_to_parse.split("articleBody")
-y = x[1].split("https://www.arcgis.com/a")[0]
-final_string = y.split("Courtesy: DataLEADS")[1]
-tot_cases_dh_str = re.findall(r"Total number of positive cases in India: \d+",final_string)
-tot_death_dh_str = re.findall(r"Total deaths in India: \d+",final_string)
-header=["Sno","State","Cases"]
-dh_table=pd.DataFrame(header)
-for idx, word in enumerate(states):
-    temp=re.findall(r"{0}: \d+".format(word),final_string)
-    if(len(temp)==0):
-        df_t=[str(idx+1),word,"0"]
-        dh_table=dh_table.append(pd.DataFrame(df_t))
-    else:
-        temp2=temp[0].split(":")
-        df_t=[str(idx+1),temp2[0],temp2[1]]
-        dh_table=dh_table.append(pd.DataFrame(df_t))
-dh_tot_cases = [int(i) for i in tot_cases_dh_str[0].split() if i.isdigit()]
-dh_tot_death = [int(i) for i in tot_death_dh_str[0].split() if i.isdigit()]
+# web_response = requests.get(dh_url)
+# txt_to_parse = web_response.text
+# x = txt_to_parse.split("articleBody")
+# y = x[1].split("https://www.arcgis.com/a")[0]
+# final_string = y.split("Courtesy: DataLEADS")[1]
+# tot_cases_dh_str = re.findall(r"Total number of positive cases in India: \d+",final_string)
+# tot_death_dh_str = re.findall(r"Total deaths in India: \d+",final_string)
+# header=["Sno","State","Cases"]
+# dh_table=pd.DataFrame(header)
+# for idx, word in enumerate(states):
+# 	temp=re.findall(r"{0}: \d+".format(word),final_string)
+# 	if(len(temp)==0):
+# 		df_t=[str(idx+1),word,"0"]
+# 		dh_table=dh_table.append(pd.DataFrame(df_t))
+# 	else:
+# 		temp2=temp[0].split(":")
+# 		df_t=[str(idx+1),temp2[0],temp2[1]]
+# 		dh_table=dh_table.append(pd.DataFrame(df_t))
+# dh_tot_cases = [int(i) for i in tot_cases_dh_str[0].split() if i.isdigit()]
+# dh_tot_death = [int(i) for i in tot_death_dh_str[0].split() if i.isdigit()]
 #Worldometer
 worldo_table = pd.read_html(worldo_url)
 worldo_df = worldo_table[0]
 worldo_india_row = worldo_df.loc[worldo_df['Country,Other'] == 'India']
-worldo_tot_cases = worldo_india_row.TotalCases
-worldo_tot_death = worldo_india_row.TotalDeaths #must change this after reading worldo json
+worldo_tot_cases = int(worldo_india_row.TotalCases)
+worldo_tot_death = int(worldo_india_row.TotalDeaths) 
+worldometer_data = 'infected : ' + str(worldo_tot_cases) + '\ndeaths : ' + str(worldo_tot_death)
+
 #MOH
-web_response = requests.get(moh_url)
-element_tree = lxml.html.fromstring(web_response.text)
-moh_table=element_tree.xpath('//tr/td//text()')
-moh_tot_cases=0
-moh_tot_deaths=0
-tmp_moh_table=moh_table
-moh_table=pd.DataFrame(header)
-conf_case=0
-idx=1
-for i in range(0,len(tmp_moh_table)):
-    if(i%6==0):
-         sno=tmp_moh_table[i]
-    elif (i%6==1):
-        state_name=tmp_moh_table[i]
-        if(state_name=="Total number of confirmed cases in India"):
-            continue
-    elif (i%6==2):
-        if(state_name=="Total number of confirmed cases in India"):
-            continue
-        if(tmp_moh_table[i]=='\n        '):
-            tmp_moh_table[i]='0'
-        data_item = [str(p) for p in tmp_moh_table[i]]
-        conf_case = conf_case + int(data_item[0])
-    elif(i%6==3):
-        if(state_name=="Total number of confirmed cases in India"):
-            continue
-        if(tmp_moh_table[i]=='\n        '):
-            tmp_moh_table[i]='0'
-        data_item = [str(p) for p in tmp_moh_table[i]]
-        conf_case = conf_case + int(data_item[0])
-    elif(i%6==4):
-        if(state_name=="Total number of confirmed cases in India"):
-            continue
-        reco_case = tmp_moh_table[i]
-    elif(i%6==5):
-        if(state_name=="Union Territory of Ladakh"):
-            state_name="Ladakh"
-        if(state_name=="Union Territory of Chandigarh"):
-            state_name="Chandigarh"
-        if(state_name=="Union Territory of Jammu and Kashmir"):
-            state_name="Jammu and Kashmir"
-        if(state_name=="Total number of confirmed cases in India"):
-            continue
-        #append entry
-        df_t=[str(idx),state_name,str(conf_case)]
-        conf_case=0
-        moh_table=moh_table.append(pd.DataFrame(df_t))
-        idx = idx + 1
-#PHARMA TECH
+# web_response = requests.get(moh_url)
+# element_tree = lxml.html.fromstring(web_response.text)
+# moh_table=element_tree.xpath('//tr/td//text()')
+# moh_tot_cases=0
+# moh_tot_deaths=0
+# tmp_moh_table=moh_table
+# moh_table=pd.DataFrame(header)
+# conf_case=0
+# idx=1
+# for i in range(0,len(tmp_moh_table)):
+# 	if(i%6==0):
+# 		 sno=tmp_moh_table[i]
+# 	elif (i%6==1):
+# 		state_name=tmp_moh_table[i]
+# 		if(state_name=="Total number of confirmed cases in India"):
+# 			continue
+# 	elif (i%6==2):
+# 		if(state_name=="Total number of confirmed cases in India"):
+# 			continue
+# 		if(tmp_moh_table[i]=='\n        '):
+# 			tmp_moh_table[i]='0'
+# 		data_item = [str(p) for p in tmp_moh_table[i]]
+# 		conf_case = conf_case + int(data_item[0])
+# 	elif(i%6==3):
+# 		if(state_name=="Total number of confirmed cases in India"):
+# 			continue
+# 		if(tmp_moh_table[i]=='\n        '):
+# 			tmp_moh_table[i]='0'
+# 		data_item = [str(p) for p in tmp_moh_table[i]]
+# 		conf_case = conf_case + int(data_item[0])
+# 	elif(i%6==4):
+# 		if(state_name=="Total number of confirmed cases in India"):
+# 			continue
+# 		reco_case = tmp_moh_table[i]
+# 	elif(i%6==5):
+# 		if(state_name=="Union Territory of Ladakh"):
+# 			state_name="Ladakh"
+# 		if(state_name=="Union Territory of Chandigarh"):
+# 			state_name="Chandigarh"
+# 		if(state_name=="Union Territory of Jammu and Kashmir"):
+# 			state_name="Jammu and Kashmir"
+# 		if(state_name=="Total number of confirmed cases in India"):
+# 			continue
+# 		#append entry
+# 		df_t=[str(idx),state_name,str(conf_case)]
+# 		conf_case=0
+# 		moh_table=moh_table.append(pd.DataFrame(df_t))
+# 		idx = idx + 1
 try:
-    os.remove("MOHFW.csv")
-    os.remove("DeccanHerald.csv")
+	os.remove("MOHFW.csv")
+	os.remove("DeccanHerald.csv")
+	os.remove("Worldometer.txt")
 except:
-    pass
+	pass
+
+writeForWorldo(worldometer_data)
+
+#PHARMA TECH
 #wget.download(pharma_url, "pharma_tech.csv")
 # tmp_pharma_table = pd.read_csv('pharma_tech.csv')
 # pharma_table=pd.DataFrame(header)
@@ -120,8 +131,8 @@ except:
 #print(moh_table)
 #print(dh_table)
 #print(pharma_table)
-moh_table.to_csv("MOHFW.csv")
-dh_table.to_csv("DeccanHerald.csv")
+# moh_table.to_csv("MOHFW.csv")
+#dh_table.to_csv("DeccanHerald.csv") TODO : Both of these don't work
 #in pharma remove total get total (Total)
 #jammu dh
 #cleanup
