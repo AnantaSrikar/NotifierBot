418ddf050e7447a438aab8f6c69d52fb22b16af0
Mayank Deshpande <43036566+mayank-pq2q4@users.noreply.github.com>
2020-04-05 11:40:58 +0530

    UPDATE #1
    Added death report function

--- startSam.py
+++ startSam.py
@@ -13,12 +13,22 @@ auth.set_access_token(getTokens()["access_token"], getTokens()["access_token_sec
 
 api = tweepy.API(auth, wait_on_rate_limit = True)
 
-def updateTweets():
+def updateTweetsInfected():
 	public_tweets = api.home_timeline()
 	for tweet in public_tweets:
 		# print((tweet.author.name))
 		# print('Tweet : {}\n\n'.format(tweet.text))
 		if('COVID-19' in tweet.text or 'Covid19' in tweet.text or 'positive' in tweet.text):
+			try:
+				tweet.retweet()
+			except Exception as e:
+				print('Exception : {}'.format(e))
+				print('Tweet : {}\n\n'.format(tweet.text))
+
+def updateTweetsDead():
+	public_tweets = api.home_timeline()
+	for tweet in public_tweets:
+		if('dead' in tweet.text or 'death' in tweet.text or 'deaths' in tweet.text or 'passed away' in tweet.text or 'dies' in tweet.text):
 			try:
 				tweet.retweet()
 			except Exception as e:
@@ -31,7 +41,9 @@ def updateTweets():
 
 def main():
 	while True:
-		updateTweets()
+		updateTweetsInfected()
+		time.sleep(5)
+		updateTweetsDead()
 		time.sleep(60 * minutes)
 
 if __name__ == '__main__':
